eb43974312590953ea0535c3d5ac6d96431f3a66
Ahmed Faiyaz <69722755+afaiyaz006@users.noreply.github.com>
2022-09-19 20:04:42 +0600

    updated

--- artnet/artnet/settings.py
+++ artnet/artnet/settings.py
@@ -121,3 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL="/media/"
+MEDIA_ROOT=BASE_DIR/"media"
+LOGIN_REDIRECT_URL = '/'
