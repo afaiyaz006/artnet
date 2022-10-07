c2db73d6d9febf09778093b65815f2cd09ae4624
Ahmed Faiyaz <69722755+afaiyaz006@users.noreply.github.com>
2022-10-07 22:17:37 +0600

    added profile view and create artwork view

--- artnet/artnet_app/views.py
+++ artnet/artnet_app/views.py
@@ -29,3 +29,64 @@ def home(request):
     page_number=request.GET.get('page')
     page_obj=paginator.get_page(page_number)
     return render(request,'artnetapp/home.html',{'artworks':artworks,'page_obj':page_obj})
+
+@login_required
+def profileview(request):
+    '''
+    Show User profile
+    '''
+    artworks=ArtWork.objects.filter(author=request.user.id)
+    artstyles=ArtStyle.objects.filter(art_author=request.user.id)
+    
+    return render(request,'artnetapp/profile.html',{'artworks':artworks,'artstyles':artstyles})
+
+@login_required
+def create_artwork_view(request):
+    """Process images uploaded by users"""
+    print(request.user)
+    
+    if request.method == 'POST':
+       
+        form = Imageform(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            # Get the current artwork instance object to display in the template
+            form_obj=form.instance
+            artwork_name=form.cleaned_data['artwork_name']
+            
+            ordinary_image_url=form_obj.ordinary_image.path
+            artwork_image_url=form_obj.artwork_image.path
+
+            artstyle_image=form_obj.artwork_image
+            
+            created_artwork=process_image(ordinary_image_url,artwork_image_url)#api call need to be carefull
+            user_instance = request.user
+            #if 'http://' not in get_artwork_url:
+            #    return render(request, 'artnetapp/artwork_creation_unsuccessfull.html')
+            artwork = ArtWork()
+            artstyle = ArtStyle()
+            artwork.name = artwork_name
+            artwork.author = user_instance
+    
+            if created_artwork:
+                # first we are saving the style
+                artstyle.art_author=user_instance
+                artstyle.style_name=artwork.name+" style "
+                artstyle.artStyle_image=artstyle_image
+                artstyle.save()
+                
+                #associating artstyle with artworks
+                artwork.artstyle_used=artstyle
+
+                #saving the artwork
+                artwork_file_name=str(artwork_name+".jpg")
+                artwork.artwork_image.save(artwork_file_name,ContentFile(image_to_byte(created_artwork),name=artwork_file_name),save=True)
+                
+                return render(request, 'artnetapp/artwork_creation_successfull.html', {'artwork':artwork})
+            else:
+                return render(request, 'artnetapp/artwork_creation_unsuccessfull.html')
+             
+    else:
+        form=Imageform()
+        
+    return render(request, 'artnetapp/artwork_create.html', {'form': form})
