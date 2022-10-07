53f579e24350e318c075004d47a55b6a2ae269e7
Ahmed Faiyaz <69722755+afaiyaz006@users.noreply.github.com>
2022-10-07 22:10:28 +0600

    added necessary imports

--- artnet/artnet_app/views.py
+++ artnet/artnet_app/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
 
-# Create your views here.
-
-
+from django.shortcuts import render,get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login,authenticate
+from .artengine import process_image,image_to_byte
+from artnet_app.forms import Imageform,ArtWork_with_selected_artstyle_form, SignupForm,ArtStyleForm
+from .models import ArtComment, ArtWork,ArtStyle,SimpleImageUpload,Profile
+from django.core.files.base import ContentFile
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.shortcuts import render,redirect
+from .forms import SignupForm,ArtWork_with_Famous_ArtStyle
+from django.core.paginator import Paginator
