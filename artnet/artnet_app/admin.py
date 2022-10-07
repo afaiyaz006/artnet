5c02673f42427a19e402d32c64ae823e671536c1
Ahmed Faiyaz <69722755+afaiyaz006@users.noreply.github.com>
2022-10-08 00:07:33 +0600

    registered the models to admin panel

--- artnet/artnet_app/admin.py
+++ artnet/artnet_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ArtWork,ArtComment,ArtStyle,Profile
+# for registering model to admin 
+admin.site.register(Profile)
+admin.site.register(ArtWork)
+admin.site.register(ArtComment)
+admin.site.register(ArtStyle)
 
-# Register your models here.
